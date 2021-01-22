d025c115ba99dbdfccb2a0d27252440526c4c670
Daniel Bennett <daniel@thebikeclub.co.uk>
2021-01-22 01:37:33 +0000

    mobile complete

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const accordions = document.getElementsByClassName("accordion");
-const icons = document.getElementsByClassName("fa-chevron-down");
+const accIcon = document.querySelectorAll(".accordion");
+const accTitle = document.querySelectorAll(".accordion__title");
+
+//
+accIcon.forEach((icon) => {
+  icon.addEventListener("click", (e) => {
+    const content = icon.nextElementSibling;
+    if (content.style.maxHeight) {
+      icon.classList.remove("active");
+    } else {
+      icon.classList.add("active");
+    }
+  });
+});
+
+accTitle.forEach((title) => {
+  title.addEventListener("click", (e) => {
+    const t = title.classList;
+
+    if (!t.contains("title-active")) {
+      t.add("title-active");
+    } else {
+      t.remove("title-active");
+    }
+  });
+});
 
 for (let i = 0; i < accordions.length; i++) {
   accordions[i].addEventListener("click", function () {
     const content = accordions[i].nextElementSibling;
-
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
