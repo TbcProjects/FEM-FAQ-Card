13b6f3c4ef8e8772d48c4c7ab125705ff928992a
Daniel Bennett <daniel@thebikeclub.co.uk>
2021-01-23 17:09:39 +0000

    icon and title functionality

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 const accordions = document.getElementsByClassName("accordion");
-// const accIcon = document.querySelectorAll(".accordion");
 const accTitle = document.querySelectorAll(".accordion__title");
+const accIcon = document.querySelectorAll(".accordion__icon");
 
+// query accordion, title and icon
+// create master function to call on listener
 
+for (const acc of accordions) {
+  acc.addEventListener("click", openAccordion);
 
+  function openAccordion() {
+    openContent();
+    highLight();
+    rotateIcon();
+  }
 
-//
-// accIcon.forEach((icon) => {
-//   icon.addEventListener("click", (e) => {
-//     const content = icon.nextElementSibling;
-//     if (content.style.maxHeight) {
-//       icon.classList.remove("active");
-//     } else {
-//       icon.classList.add("active");
-//     }
-//   });
-// });
-
-accTitle.forEach((title) => {
-  title.addEventListener("click", (e) => {
-    const t = title.classList;
-    if (t.contains("title-active")) {
-      t.remove("title-active");
-    } else {
-      t.add("title-active");
-    }
-  });
-});
-
-for (let i = 0; i < accordions.length; i++) {
-  accordions[i].addEventListener("click", function () {
-    const content = accordions[i].nextElementSibling;
+  const openContent = () => {
+    let content = acc.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
-  });
+  };
+
+  const highLight = () => {
+    accTitle.forEach((title) => {
+      let classes = title.classList;
+      if (classes.contains("title-active")) {
+        classes.remove("title-active");
+      } else {
+        classes.add("title-active");
+      }
+    });
+  };
+
+  const rotateIcon = () => {
+    accIcon.forEach((icon) => {
+      let classes = icon.classList;
+      if (classes.contains("icon-active")) {
+        classes.remove("icon-active");
+      } else {
+        classes.add("icon-active");
+      }
+    });
+  };
 }
+
+// Checkers
+console.log(accIcon);
