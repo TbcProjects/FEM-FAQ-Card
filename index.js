127a79294461f4489fdc620929575a026e371a82
Daniel Bennett <daniel@thebikeclub.co.uk>
2021-01-24 03:14:46 +0000

    fix title and icon functionality

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
 const accordions = document.getElementsByClassName("accordion");
-const accTitle = document.querySelectorAll(".accordion__title");
-const accIcon = document.querySelectorAll(".accordion__icon");
-
-// query accordion, title and icon
-// create master function to call on listener
 
 for (const acc of accordions) {
   acc.addEventListener("click", openAccordion);
 
-  function openAccordion() {
-    openContent();
-    highLight();
-    rotateIcon();
-  }
-
-  const openContent = () => {
+  //
+  function openAccordion(e) {
     let content = acc.nextElementSibling;
-    if (content.style.maxHeight) {
+    let mHeight = content.style.maxHeight;
+    let title = acc.firstElementChild;
+    let icon = acc.lastElementChild;
+
+    // open content
+    if (mHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
-  };
+    // make title bold when active
+    if (!mHeight) {
+      title.classList.add("title-active");
+    } else {
+      title.classList.remove("title-active");
+    }
 
-  const highLight = () => {
-    accTitle.forEach((title) => {
-      let classes = title.classList;
-      if (classes.contains("title-active")) {
-        classes.remove("title-active");
-      } else {
-        classes.add("title-active");
-      }
-    });
-  };
-
-  const rotateIcon = () => {
-    accIcon.forEach((icon) => {
-      let classes = icon.classList;
-      if (classes.contains("icon-active")) {
-        classes.remove("icon-active");
-      } else {
-        classes.add("icon-active");
-      }
-    });
-  };
+    // rotate chevron 180deg
+    if (!mHeight) {
+      icon.classList.add("icon-active");
+    } else {
+      icon.classList.remove("icon-active");
+    }
+  }
 }
 
-// Checkers
-console.log(accIcon);
+
